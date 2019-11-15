fff86495af041954f63ec38f07119d2bdde1a31f
Rado13 <dsadsa55@centrum.sk>
2019-11-15 15:32:27 +0100

    implemented firebase , store authenticated users

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,32 @@ const config = {
     appId: "1:700194720151:web:c85322011e8ccd15505cd2"
   }
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+
+    return userRef;
+};
+
   firebase.initializeApp(config);
   //veci na google authen
 
