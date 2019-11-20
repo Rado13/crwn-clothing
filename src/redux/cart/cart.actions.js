31d98d5d419f31eb6baf85147f89c32519eab9d7
Rado13 <dsadsa55@centrum.sk>
2019-11-20 14:20:23 +0100

    updated shop data, updated cartUtils to group items in cartReducer

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -4,3 +4,8 @@ export const toggleCartHidden = () => ({
     type: CartActionTypes.TOGGLE_CART_HIDDEN
     //payload nemusim, nepouzivam ho v cart.reducer ani
 });
+
+export const addItem = item => ({
+    type: CartActionTypes.ADD_ITEM,
+    payload: item
+});
